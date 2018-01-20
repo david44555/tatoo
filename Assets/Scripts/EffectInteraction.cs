560029ea4c67b251cbcc6a6aa9711532db959a0e
david44555 <33566400+david44555@users.noreply.github.com>
2018-01-20 13:40:30 +0300

    Add files via upload

---
+++ Assets/Scripts/EffectInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectInteraction : MonoBehaviour {
+
+    public float radiusToTarget = 3;
+    GameObject[] targets;
+    
+	void Start()
+    {
+        targets = GameObject.FindGameObjectsWithTag("effect");
+    }
+
+    void Update()
+    {
+        foreach(var effect in targets)
+        {
+            if (gameObject == effect)
+                continue;
+            if (Vector3.Distance(transform.position, effect.transform.position) < radiusToTarget)
+            {
+                Debug.Log("Лалка");
+                var effectAction = GetComponent<IEffect>();
+                if (effectAction != null)
+                    effectAction.Run();
+
+            }
+        }
+
+    }
+
+}
